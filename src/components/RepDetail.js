f1e1b417a18e20276623530babad51e257c5e407
Tanvir Islam <islamtanvir8991@gmail.com>
2018-11-04 14:57:22 -0500

    Copy pasted working file from computer

--- src/components/RepDetail.js
+++ src/components/RepDetail.js
@@ -3,37 +3,37 @@ import { Text, View, Image } from'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
-
 const RepDetail = ({ record }) => {
 
     // we destructure here becuase we had props.record.(something) over 
     // and over so we make it nice and clean 
     // const { repName, repParty, repImage } = props;
-
+    const id = record.id;
 
     return (
         <Card>
             <CardSection>
-                {/* right now we dont have an image to work with, so leave commented */}
                 {/* Image of your Rep */}
                 <View style={styles.imageContainerStyle}>
                     <Image
-                        syle={styles.repImageStyle}
+                        style={styles.repImageStyle}
                         source={{uri: 'https://theunitedstates.io/images/congress/225x275/' + id + '.jpg'}}
                     />
                 </View>
-
+                
+                {/* {console.log('yo whats up')}; */}
                 {/* Name of Rep & party */}
                 <View style={styles.repInfoStyle}>
                     <Text style={styles.repNameStyle}>{record.name}</Text>
-                    <Text>{repcord.party}</Text>
+                    <Text>{record.party}</Text>
                 </View>
+            </CardSection>
 
-                <CardSection>
-                    <Button>
-                        Call
-                    </Button>
-                </CardSection>
+            {/* Now we need our button to actually call the reps */}
+            <CardSection>
+                <Button>
+                    Call
+                </Button>
             </CardSection>
         </Card>
     );
@@ -43,7 +43,7 @@ const styles ={
     repImageStyle:
     {
         height: 50, 
-        width: 50
+        width: 50,
     },
     imageContainerStyle:
     {
