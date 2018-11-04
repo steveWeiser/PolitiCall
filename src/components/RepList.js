e31086658de76405a3e6344b639b8053944fde2d
Tanvir Islam <islamtanvir8991@gmail.com>
2018-11-03 20:28:17 -0400

    Refactor RepList for functioning API Call using axios

--- src/components/RepList.js
+++ src/components/RepList.js
@@ -1,30 +1,43 @@
 import React from 'react';
-import { ScrollView, Text} from 'react-native';
+import { ScrollView } from 'react-native';
 import RepDetail from './RepDetail';
+import axios from 'axios';
 
-// this needs to be refactored after API's are up 
-// this is a functional component and can't fetch data
-const RepList = () => {
-    return (
-        <ScrollView>
-            <RepDetail 
-                repName={'Barney Williams'} 
-                repParty={'Goofy Goobers'}
-                repImage={'https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/425912_10151040658500925_824590736_n.jpg?_nc_cat=106&_nc_ht=scontent-mia3-1.xx&oh=d49bb33ac353f503ed41c7e9a5828c73&oe=5C568FCD'}
-            />
-            <RepDetail 
-                repName={'Indian Jones'} 
-                repParty={'Goofy Goobers'}
-                repImage={'https://www.coupdemainmagazine.com/sites/default/files/styles/full_width/public/article/8846/hero-8846-372122306.jpg?itok=078MizYc'}
-            />
-            <RepDetail 
-                repName={'Stephen Colbert'} 
-                repParty={'Goofy Goobers'}
-                repImage={'https://upload.wikimedia.org/wikipedia/commons/7/7f/Stephen_Colbert_December_2017.jpg'}
-            />
+// this is class based because it needs to make an https request 
+class RepList extends Component {
+    // set our default state of empty reps 
+    state = { reps : [] }; 
 
-        </ScrollView>
-    );
-};
+    // will execute as soon as its rendered to the screen 
+    componentWillMount () {
+        var header = {
+            'Content-Type': 'application/json',
+            'X-API-Key': 'KDNMUgMhKjpCYBUiTj6nV6eO3IugCr7OIBUk6kSP'
+        };
+        axios.get('https://api.propublica.org/congress/v1/members/senate/FL/current.json',{headers: header}).then(response => this.setState({ reps: response.data.results }));
+    }
 
+    renderReps() {
+        // map is an array helper 
+        // pass in fat arrow function, that is called for each rep 
+        // what is returned is put into a new array 
+        // rep.title is a js variable within JSX so you need curly braces 
+        return this.state.reps.map(rep => 
+            // best property to use is to use an ID 
+            // we want a prop of 'rep' to be passed into each repdetail component 
+            // prop name = record 
+            // variable name = rep
+            <RepDetail key={rep.id} record={rep} />
+        );
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                {this.renderReps()}
+                {/* <Text>{JSON.stringify(this.state)}</Text> */}
+            </ScrollView>
+        );
+    }
+}
 export default RepList;
