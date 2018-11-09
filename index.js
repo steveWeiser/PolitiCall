ec822895cd02f263b6065d091d9c2df3c9c8f577
Tanvir Islam <islamtanvir8991@gmail.com>
2018-11-08 19:50:56 -0500

    added navbar to index

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ import { AppRegistry, Text, View } from 'react-native';
 import Header from './src/components/Header';
 //import our list 
 import RepList from './src/components/RepList';
+import Navbar from './src/components/Navbar';
 const App = () => {
     return (
         <View style={{flex: 1}}>
             <Header headerText={'PolitiCall'}/>
-            <RepList/>
+            {/* <RepList/> */}
+            <Navbar/>
         </View>
         
     );
